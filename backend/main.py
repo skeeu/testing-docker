fc569844b3c0cc94b711b9290a2a6984e240c58a
skeeu <bisembaev.arman@gmail.com>
2023-07-30 19:18:57 +0600

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+
 app = FastAPI(root_path="/api")
 
 
 @app.get("/")
 async def index():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello")
+async def hello():
     return {"message": "Hello World"}
